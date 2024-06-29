38abe330878e24788467fc9deeb4930e04cacf10
ziifou <khelafi.sifeddine@gmail.com>
2024-06-29 19:16:34 +0200

    project init

---
+++ src/main/java/com/sif/microserviceApp/MicroserviceAppApplication.java
@@ -0,0 +1,13 @@
+package com.sif.microserviceApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sif/microserviceApp/controllers/AccountsController.java
@@ -0,0 +1,14 @@
+package com.sif.microserviceApp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountsController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+    //post method
+    pu
+}
