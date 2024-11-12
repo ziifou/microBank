7021a4276d8bb7e3b839e2730278a3f46cd2f5da
ziifou <khelafi.sifeddine@gmail.com>
2024-11-12 18:02:13 +0100

    add config

---
+++ cards/src/main/java/com/sif/cards/CardsApplication.java
@@ -0,0 +1,39 @@
+package com.sif.cards;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards microservice REST API Documentation",
+				description = "EazyBank Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Madan Reddy",
+						email = "tutor@sif.com",
+						url = "https://www.sif.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.sif.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "EazyBank Cards microservice REST API Documentation",
+				url = "https://www.sif.com/swagger-ui.html"
+		)
+)
+public class CardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApplication.class, args);
+	}
+}
---
+++ loans/src/main/java/com/sif/loans/LoansApplication.java
@@ -0,0 +1,42 @@
+package com.sif.loans;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.sif.loans.controller") })
+@EnableJpaRepositories("com.sif.loans.repository")
+@EntityScan("com.sif.loans.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Loans microservice REST API Documentation",
+				description = "EazyBank Loans microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Madan Reddy",
+						email = "tutor@sif.com",
+						url = "https://www.sif.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.sif.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "EazyBank Loans microservice REST API Documentation",
+				url = "https://www.sif.com/swagger-ui.html"
+		)
+)
+public class LoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansApplication.class, args);
+	}
+}
