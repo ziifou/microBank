e5cbca3c4332657eb72868c8dfb9b0a7ed90b0dd
ziifou <khelafi.sifeddine@gmail.com>
2024-11-28 15:57:10 +0100

    add netflix eureka

---
+++ eurekas/src/main/java/sif/eur/eurekas/EurekasApplication.java
@@ -0,0 +1,15 @@
+package sif.eur.eurekas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekasApplication.class, args);
+	}
+
+}
---
+++ eurekas/src/test/java/sif/eur/eurekas/EurekasApplicationTests.java
@@ -0,0 +1,13 @@
+package sif.eur.eurekas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
