ca7b1b53748a39b0cec44371edbf4889e2c073d6
ziifou <khelafi.sifeddine@gmail.com>
2024-11-28 17:36:24 +0100

    fix bug not enable to create a customer due to auditor absence

--- accounts/src/main/java/com/sif/microserviceApp/AccountApplication.java
+++ accounts/src/main/java/com/sif/microserviceApp/AccountApplication.java
@@ -4,9 +4,13 @@ import com.sif.microserviceApp.dto.AccountsContactInfoDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableFeignClients
 public class AccountApplication {
 
 	public static void main(String[] args) {
