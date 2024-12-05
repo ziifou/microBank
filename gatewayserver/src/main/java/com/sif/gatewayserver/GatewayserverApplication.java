2e59ccc605457e42ed9c62125ba503715b85329b
ziifou <khelafi.sifeddine@gmail.com>
2024-12-05 10:47:12 +0100

    add gateway server

---
+++ gatewayserver/src/main/java/com/sif/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,14 @@
+package com.sif.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+
+}
